fa2b800fd366f867bcbbdea6ad8b2f78f71a10ac
Laura Felce <crepu83@yahoo.it>
2019-06-09 14:34:39 +0000

    Automatically backed up by Learn

--- app/models/concerns/methods.rb
+++ app/models/concerns/methods.rb
@@ -1,6 +1,7 @@
 
-module Reservable
-   extend ActiveSupport::Concern
+module Methods
+
+  module InstanceMethods
 
   def openings(start_date, end_date)
     openings = []
@@ -14,25 +15,21 @@ module Reservable
     end
   end
 
-  def ratio_reservations_to_listings
-    if listings.count > 0
-      reservations.count.to_f / listings.count.to_f
+  module ClassMethods
+    def highest_ratio_res_to_listings
+      self.has_listings.max_by {|location| location.reservations.count / location.listings.count }
     end
+
+    def most_res
+      self.has_listings.max_by {|location| location.reservations.count }
+    end
+
+    def has_listings
+      self.all.select {|location| location.listings.any? }
+    end
+
   end
 
-  class_methods do
 
-    def highest_ratio_reservations_to_listings
 
-      all.max do |a, b|
-        a.ratio_reservations_to_listings <=> b.ratio_reservations_to_listings
-      end
-    end
-
-    def most_reservations
-      all.max do |a, b|
-        a.reservations.count <=> b.reservations.count
-      end
-    end
-  end
 end
