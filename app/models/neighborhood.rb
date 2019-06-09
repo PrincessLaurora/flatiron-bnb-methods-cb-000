8f9639cbcc5e4c2d4134c4d954743c46b81dfb38
Laura Felce <crepu83@yahoo.it>
2019-06-09 14:36:50 +0000

    Automatically backed up by Learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,10 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  has_many :reservations, :through => :listings
+
+  include Methods::InstanceMethods
+  extend Methods::ClassMethods
+
 
 end
