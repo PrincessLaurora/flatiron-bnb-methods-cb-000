981dbaa905f2698c7d066dfeaa9e19921c1ee851
Laura Felce <crepu83@yahoo.it>
2019-06-09 14:34:33 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,4 +3,12 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
   has_many :reservations, :through => :listings
 
+  include Methods::InstanceMethods
+  extend Methods::ClassMethods
+
+   def city_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
+end
+
 end
